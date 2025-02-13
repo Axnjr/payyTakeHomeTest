974d3be910140e89680197b6f1999a5ac63049b1
Yakshit Chhipa <yakshitchhipa@gmail.com>
2025-02-13 18:25:00 +0530

    feat: implement async ticker and line reader with LendingIterator trait

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,62 @@
-use std::{thread, time::Duration};
+use std::{sync::{atomic::{AtomicBool, AtomicUsize, Ordering}, Arc, Mutex}, task, thread, time::Duration};
+use tokio;
 
-fn ticker<F>(mut func: F) where F: FnMut() + Send + 'static, {
-    loop {
-        func();
-        thread::sleep(Duration::from_secs(1));
-    }
+mod line_reader;
+
+struct Ticker {
+    count: Arc<AtomicUsize>,
+    running: Arc<AtomicBool>
 }
 
-fn main() {
-    let mut counter = 0;
+// async fn ticker<F>(mut func: F) where F: FnMut() + Send + 'static, {
+//     loop {
+//         func();
+//         // thread::sleep(Duration::from_secs(1));
+//         tokio::time::sleep(Duration::from_secs(1)).await; // changed
+//     }
+// }
 
-    ticker(move || {
-        println!("Ticker executing the closure of the main function. Counter: {}", counter);
-        counter += 1;
-    });
+impl Ticker {
+
+    pub fn new() -> Self {
+        Ticker {
+            count: Arc::new(AtomicUsize::new(0)),
+            running: Arc::new(AtomicBool::new(true))
+        }
+    }
+
+    pub async fn start(&self) {
+
+        let count_clone = Arc::clone(&self.count);
+        let running_clone = Arc::clone(&self.running);
+
+        tokio::spawn(async move {
+            while running_clone.load(Ordering::Acquire) {
+                let current_count = count_clone.fetch_add(1, Ordering::Relaxed);
+                println!(
+                    "Ticker executing the closure of the main function. Counter: {}",
+                    current_count
+                );
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            }
+        });
+    }
+
+    pub async fn stop(&self) {
+        let _ = &self.running.store(false, Ordering::Release);
+    }
+
+}
+
+#[tokio::main]
+async fn main() {
+    let ticker = Ticker::new();
+
+    ticker.start().await;
+
+    println!("MAIN THREAD IS WORKING INDEED !! IT WILL SLEEP FOR 5 SEC");
+
+    tokio::time::sleep(Duration::from_secs(5)).await;
+
+    ticker.stop().await;
 }
